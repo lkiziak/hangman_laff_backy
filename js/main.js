778b2a2a6717f79b7463de01160b16818ae5d808
Michelle Kiziak <makiziak@gmail.com>
2016-10-09 18:30:47 -0700

    Front-end of spelling bee application. Need Javascript.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//setup initial state variables
+//variables defined
+//English words: apple, monkey
+//Spanish word: manzana, mono
+
+var words = document.getElementsByClassName("apple", "monkey", "car")
+var spanish = document.getElementsByClassName("manzana", "mono", "carro")
+var clear = document.getElementsByClassName("clear")
+
+
+
+
+
+//setup eventListeners
+
+
+
+
+
+
+
+
+//Functions for word find
+
+
+
+
+
+
