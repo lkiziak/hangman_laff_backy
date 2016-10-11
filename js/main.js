ec8518f85b8d499ebffb3518f9c97d3d4d1b3b15
Michelle Kiziak <makiziak@gmail.com>
2016-10-11 14:04:28 -0700

    updates on my updates

--- js/main.js
+++ js/main.js
@@ -10,15 +10,22 @@
 // FUNCTION: for (var i = 0; i < words[i].length; i++) {
 //   $('#english').text('_ ');
 // }
-//newGame()
-//document.getElementById("button").onclick = guessLetter;
-//
+
+
+//function for letters to come up on screen
+//have operting buttons function on screen
+
 
 var words = ["apple", "monkey", "car", "school", "house"];
 var spanWords = ["manzana", "mono", "carro", "escuela", "casa"];
 var newWordBlanks = "";
 var guessLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var word = {};
+var $display = $("#display");
+var $clear = $("#clear");
+var $play = $("#play");
+var $newGame = $("#new");
+var MaxGuesses = "";
 
 
 // takes a random word, sets it to my word object, and saves the index in that
@@ -38,20 +45,46 @@ function newWordBlanks (word) {
   return blanks
 }
 
-//guess letters
-function guessLetters () {
-var letters = [];
-  if (guess === null) {
-    break;
-  } else if (guess.length != 1) {
-    alert("Try again.");
-  } else {
-    for (var j = 0; j < word.length; j++);
-      if (words[j] === guess) {
-        blanks[j] === guess;
-     }
-    }
-}
+$('.letter').on('click', function(e) {
+  console.log(e.target.value);
+})
+
+$('#clear').on('click', function(e) {
+  console.log(e.target.value);
+})
+
+$('#play').on('click', function(e) {
+  console.log(e.target.value);
+})
+
+$('#new').on('click', function(e) {
+  console.log(e.target.value);
+})
+
+$('.display').on('click', function(e) {
+  console.log(e.target.value);
+})
+
+$("#new").submit(function(event) {
+  alert("New Game!");
+});
+
+
+
+//Guess Letters
+//function guessLetters () {
+//var letters = [];
+// if (guess === null) {
+//    break;
+//  } else if (guess.length != 1) {
+//    alert("Try again.");
+//  } else {
+//    for (var j = 0; j < word.length; j++);
+//      if (words[j] === guess) {
+//        blanks[j] === guess;
+//     }
+//    }
+//}
 
 
 
